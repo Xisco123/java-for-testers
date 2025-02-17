dfcfeac1fd1dfe34c0ee848330320579d1e73988
mouloud <mouloud.bsn@gmail.com>
2025-02-16 20:41:10 -0500

    Commit class Employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Employee {
+
+    private String name;
+    private String sexe;
+    private String adress;
+    private int salary;
+    private int idEmployee;
+}
